afb25355b252775a1dba4f4c8bb0bb17480a1985
Sebika-K <khulalsebika11@gmail.com>
2025-11-06 23:06:09 -0600

    navbar addition

--- frontend/src/pages/AddPaper.jsx
+++ frontend/src/pages/AddPaper.jsx
@@ -1,13 +1,56 @@
 import React from "react";
 import AddPaperForm from "../components/AddPaperForm";
+import miniLogo from "../assets/minischolar.png";
 
 export default function AddPaper() {
   return (
-    <div className="min-h-screen bg-gray-100 p-6">
-      <div className="max-w-2xl mx-auto">
-        <h2 className="text-2xl font-bold text-black mb-4">Add a Paper</h2>
-        <AddPaperForm />
+    <div style={{ backgroundColor: "black", minHeight: "100vh" }}>
+      {/* NAVBAR*/}
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+          padding: "1rem 1rem",
+          backgroundColor: "black",
+        }}
+      >
+        {/* LEFT — LOGO */}
+        <div style={{ display: "flex", alignItems: "center", gap: "1rem" }}>
+          <img
+            src={miniLogo}
+            alt="Scholar Graph"
+            style={{ height: "150px", width: "150px" }}
+          />
+        </div>
+
+        {/* RIGHT — LINKS */}
+        <div style={{ display: "flex", alignItems: "center", gap: "2rem" }}>
+          <a
+            href="/"
+            style={{
+              color: "#A39A8D",
+              fontWeight: "500",
+              textDecoration: "underline",
+              textUnderlineOffset: "4px",
+            }}
+          >
+            Home
+          </a>
+          <a
+            href="/search"
+            style={{
+              color: "#A39A8D",
+              fontWeight: "500",
+              textDecoration: "underline",
+              textUnderlineOffset: "4px",
+            }}
+          >
+            Search A Paper
+          </a>
+        </div>
       </div>
+      <AddPaperForm />
     </div>
   );
 }
