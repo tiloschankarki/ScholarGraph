e50871768dd93a21125066078346bc499eb70dd0
Sebika-K <khulalsebika11@gmail.com>
2025-11-06 23:14:08 -0600

    tailwind transition

--- frontend/src/pages/AddPaper.jsx
+++ frontend/src/pages/AddPaper.jsx
@@ -6,49 +6,30 @@ export default function AddPaper() {
   return (
     <div style={{ backgroundColor: "black", minHeight: "100vh" }}>
       {/* NAVBAR*/}
-      <div
-        style={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "space-between",
-          padding: "1rem 1rem",
-          backgroundColor: "black",
-        }}
-      >
+      <div className="flex items-center justify-between px-12 py-4 bg-black">
         {/* LEFT — LOGO */}
-        <div style={{ display: "flex", alignItems: "center", gap: "1rem" }}>
-          <img
-            src={miniLogo}
-            alt="Scholar Graph"
-            style={{ height: "150px", width: "150px" }}
-          />
-        </div>
-
-        {/* RIGHT — LINKS */}
-        <div style={{ display: "flex", alignItems: "center", gap: "2rem" }}>
-          <a
-            href="/"
-            style={{
-              color: "#A39A8D",
-              fontWeight: "500",
-              textDecoration: "underline",
-              textUnderlineOffset: "4px",
-            }}
-          >
-            Home
-          </a>
-          <a
-            href="/search"
-            style={{
-              color: "#A39A8D",
-              fontWeight: "500",
-              textDecoration: "underline",
-              textUnderlineOffset: "4px",
-            }}
-          >
-            Search A Paper
-          </a>
-        </div>
+          <div className="flex items-center gap-4">
+            <img
+              src={miniLogo}
+              alt="Scholar Graph"
+              className="h-[150px] w-[150px]"
+            />
+          </div>
+          {/* RIGHT — LINKS */}
+          <div className="flex items-center gap-8">
+            <a
+              href="/"
+              className="text-[#A39A8D] hover:underline hover:text-white transition-all hover:opacity-80"
+            >
+              Home
+            </a>
+            <a
+              href="/search"
+              className="text-[#A39A8D] hover:underline hover:text-white transition-all hover:opacity-80"
+            >
+              Search A paper
+            </a>
+          </div>
       </div>
       <AddPaperForm />
     </div>
